1c3ecd46c9b8f505a3fc237e982c18d320887963
Anshul Taklikar <ansh@Anshuls-Air.lan>
2024-10-25 21:52:23 +0530

    Changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React changed to working branch
         </a>
       </header>
     </div>
